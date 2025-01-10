b0449fa69c14f0f6db14d768b3a89bc8933c8af6
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-10 17:46:31 -0300

    feat: added some TextInput, styling according to class

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,41 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput, Text } from "react-native";
 
 const TextScreen = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Name:</Text>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text>My name is {name}</Text>
+
+      <Text>Enter Password:</Text>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {password.length < 5 ? <Text>Password must have at least 5 characters</Text> : null}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
 export default TextScreen;
