f7de2b2c843312ca0e623a70b23bbb0cc6628452
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-10 14:00:58 -0300

    feat: create ColorCounter component

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Button, Text } from "react-native";
+
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
+  return (
+    <View>
+      <Text>{color}</Text>
+      <Button title={`Increase ${color}`} onPress={() => onIncrease()} />
+      <Button title={`Decrease ${color}`} onPress={() => onDecrease()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorCounter;
